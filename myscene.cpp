0a60015dd11bd01a1d740fc4e0d3779ce4f853f3
LordElain <47543964+LordElain@users.noreply.github.com>
2020-01-06 10:54:18 +0100

    Mapping: Gebäude
    Rand des Spielbaren Bereichs Platziert und erste Texturen drauf gepackt

--- myscene.cpp
+++ myscene.cpp
@@ -37,7 +37,7 @@ Node *initScene1();
 ScreenRenderer* sortedRenderer;
 ScreenRenderer* preOrderRenderer;
 unsigned int myScene, myScene2;
-SoundSource *gSoundSource1, *gSoundSource2, *gSoundSource3, *gAmbientSoundSource;
+SoundSource *gSoundSource1, *gAmbientSoundSource;
 Node* gAudioListenerNode;
 
 class SceneSwitcher: public Listener
@@ -71,7 +71,7 @@ Node *initScene1()
         Node* root=new Node();
         Texture *t;
         Shader* s = ShaderManager::getShader((":/shaders/Filter.vert"), (":/shaders/Filter.frag"));
-        Shader* s1 = ShaderManager::getShader((":/shaders/Movement.vert"), (":/shaders/Filter.frag"));
+        //Shader* s1 = ShaderManager::getShader((":/shaders/Movement.vert"), (":/shaders/Filter.frag"));
 
     //Geometry
         Geometry* e_Fish = new TriangleMesh(":/objects/Pufferfish_Mob.obj");
@@ -98,6 +98,16 @@ Node *initScene1()
         Drawable* model9_Spawn3 = new Drawable(b_Spawn_rechts);
         Drawable* model10_Spawn4 = new Drawable(b_Spawn_unten);
         Drawable* model11_SDoor = new Drawable(b_Door);
+        Drawable* model12_Small2 = new Drawable(b_Small);
+        Drawable* model13_Middle2 = new Drawable(b_Medium);
+        Drawable* model14_Tall2 = new Drawable(b_Tall);
+        Drawable* model15_Tall3 = new Drawable(b_Tall);
+        Drawable* model16_Tall4 = new Drawable(b_Tall);
+        Drawable* model17_Small3 = new Drawable(b_Small);
+        Drawable* model18_Small4 = new Drawable(b_Small);
+        Drawable* model19_Middle3 = new Drawable(b_Medium);
+        Drawable* model20_Tall5 = new Drawable(b_Tall);
+        Drawable* model21_Small5 = new Drawable(b_Small);
 
     //Transformation
         //Mobs
@@ -132,13 +142,44 @@ Node *initScene1()
         Transformation* b_TSmall = new Transformation();
         Node* transformationBuilding1 = new Node(b_TSmall);
 
+        Transformation* b_TSmall2 = new Transformation();
+        Node* transformationBuilding4 = new Node(b_TSmall2);
+
+        Transformation* b_TSmall3 = new Transformation();
+        Node* transformationBuilding9 = new Node(b_TSmall3);
+
+        Transformation* b_TSmall4 = new Transformation();
+        Node* transformationBuilding10 = new Node(b_TSmall4);
+
+        Transformation* b_TSmall5 = new Transformation();
+        Node* transformationBuilding13 = new Node(b_TSmall5);
+
+
         //Middle
         Transformation* b_TMiddle = new Transformation();
         Node* transformationBuilding2 = new Node(b_TMiddle);
 
+        Transformation* b_TMiddle2 = new Transformation();
+        Node* transformationBuilding5 = new Node(b_TMiddle2);
+
+        Transformation* b_TMiddle3 = new Transformation();
+        Node* transformationBuilding11 = new Node(b_TMiddle3);
+
         //Tall
         Transformation* b_TTall = new Transformation();
         Node* transformationBuilding3 = new Node (b_TTall);
+
+        Transformation* b_TTall2 = new Transformation();
+        Node* transformationBuilding6 = new Node (b_TTall2);
+
+        Transformation* b_TTall3 = new Transformation();
+        Node* transformationBuilding7 = new Node (b_TTall3);
+
+        Transformation* b_TTall4 = new Transformation();
+        Node* transformationBuilding8 = new Node (b_TTall4);
+
+        Transformation* b_TTall5 = new Transformation();
+        Node* transformationBuilding12 = new Node (b_TTall5);
 
     //Physiks
     int v_Slot = PhysicEngineManager::createNewPhysicEngineSlot(PhysicEngineName::BulletPhysicsLibrary);
@@ -150,13 +191,13 @@ Node *initScene1()
         PhysicObjectConstructionInfo* v_Constrinf_Mob = new PhysicObjectConstructionInfo();
         v_Constrinf_Mob->setCollisionHull(CollisionHull::SphereAABB);
         v_Constrinf_Mob->setCcdActivation(true);
-        v_Constrinf_Mob->setMidpointTransformation(QMatrix4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
+        //v_Constrinf_Mob->setMidpointTransformation(QMatrix4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
         v_Constrinf_Mob->setMass(1.f);
 
         Mob->setConstructionInfo(v_Constrinf_Mob);
         model1_Fish->getPhysicObject()->registerPhysicObject();
 
-        model1_Fish->setShader(s1);
+
         f_TPuffer->translate(10.f,0.1f,0.f);
 
 
@@ -189,8 +230,8 @@ Node *initScene1()
        model11_SDoor->getPhysicObject()->registerPhysicObject();
 
        b_TSpawn1->translate(0.f,0.05f,0.f);
-       b_TSpawn2->translate(-0.1f,0.f,0.f);
-       b_TSpawn3->translate(0.1f,0.f,0.f);
+       b_TSpawn2->translate(-0.05f,0.f,0.f);
+       b_TSpawn3->translate(0.05f,0.f,0.f);
        b_TSpawn4->translate(0.f,0.f,-1.f);
        b_TSpawn5->translate(0.f,0.f,4.f);
       // b_TSpawn5->rotate(30,0.f,1.f,0.f);
@@ -200,24 +241,64 @@ Node *initScene1()
         model5_Small->setStaticGeometry(true);
         model6_Middle->setStaticGeometry(true);
         model7_Tall->setStaticGeometry(true);
+        model12_Small2->setStaticGeometry(true);
+        model13_Middle2->setStaticGeometry(true);
+        model14_Tall2->setStaticGeometry(true);
+        model15_Tall3->setStaticGeometry(true);
+        model16_Tall4->setStaticGeometry(true);
+        model17_Small3->setStaticGeometry(true);
+        model18_Small4->setStaticGeometry(true);
+        model19_Middle3->setStaticGeometry(true);
+        model20_Tall5->setStaticGeometry(true);
+        model21_Small5->setStaticGeometry(true);
         PhysicObject* Build = v_PhysicEngine->createNewPhysicObject(model5_Small);
         Build = v_PhysicEngine->createNewPhysicObject(model6_Middle);
         Build = v_PhysicEngine->createNewPhysicObject(model7_Tall);
+        Build = v_PhysicEngine->createNewPhysicObject(model12_Small2);
+        Build = v_PhysicEngine->createNewPhysicObject(model13_Middle2);
+        Build = v_PhysicEngine->createNewPhysicObject(model14_Tall2);
+        Build = v_PhysicEngine->createNewPhysicObject(model15_Tall3);
+        Build = v_PhysicEngine->createNewPhysicObject(model16_Tall4);
+        Build = v_PhysicEngine->createNewPhysicObject(model17_Small3);
+        Build = v_PhysicEngine->createNewPhysicObject(model18_Small4);
+        Build = v_PhysicEngine->createNewPhysicObject(model19_Middle3);
+        Build = v_PhysicEngine->createNewPhysicObject(model20_Tall5);
+        Build = v_PhysicEngine->createNewPhysicObject(model21_Small5);
         PhysicObjectConstructionInfo* v_Constrinf_Build = new PhysicObjectConstructionInfo();
         v_Constrinf_Build->setMass(10.f);
-        v_Constrinf_Build->setLocalInertiaPoint(QVector3D(0.f, 0.f, 0.f));
-        v_Constrinf_Build->setCollisionHull(CollisionHull::BoxCovarianceFromPoints);
+        //v_Constrinf_Build->setLocalInertiaPoint(QVector3D(0.f, 0.f, 0.f));
+        v_Constrinf_Build->setCollisionHull(CollisionHull::BVHTriangleMesh);
         //v_Constrinf_Build->setBoxHalfExtends(QVector3D(0.5f, 0.5f, 0.5f));
         Build->setConstructionInfo(v_Constrinf_Build);        
         model5_Small->getPhysicObject()->registerPhysicObject();
         model6_Middle->getPhysicObject()->registerPhysicObject();
         model7_Tall->getPhysicObject()->registerPhysicObject();
+        model12_Small2->getPhysicObject()->registerPhysicObject();
+        model13_Middle2->getPhysicObject()->registerPhysicObject();
+        model14_Tall2->getPhysicObject()->registerPhysicObject();
+        model15_Tall3->getPhysicObject()->registerPhysicObject();
+        model16_Tall4->getPhysicObject()->registerPhysicObject();
+        model17_Small3->getPhysicObject()->registerPhysicObject();
+        model18_Small4->getPhysicObject()->registerPhysicObject();
+        model19_Middle3->getPhysicObject()->registerPhysicObject();
+        model20_Tall5->getPhysicObject()->registerPhysicObject();
+        model21_Small5->getPhysicObject()->registerPhysicObject();
 
-        b_TSmall->translate(-12.f,0.1f,14.f);
+        b_TSmall->translate(-16.f,0.1f,8.f);
         b_TSmall->rotate(90.f,0.f,1.f,0.f);
-        b_TMiddle->translate(-12.f,0.1f,22.f);
+        b_TMiddle->translate(-16.f,0.1f,16.f);
         b_TMiddle->rotate(90.f,0.f,1.f,0.f);
-        b_TTall->translate(10.0f,0.1f,-14.f);
+        b_TTall->translate(8.0f,0.1f,-8.f);
+        b_TSmall2->translate(-8.f,0.1f,0.f);
+        b_TMiddle2->translate(5.f,0.1f,13.f);
+        b_TTall2->translate(-8.f,0.1f,24.f);
+        b_TTall3->translate(0.f,0.1f,24.f);
+        b_TTall4->translate(8.f,0.1f,24.f);
+        b_TSmall3->translate(16.f,0.1f,-8.f);
+        b_TSmall4->translate(24.f,0.1f,0.f);
+        b_TMiddle3->translate(24.f,0.1f,8.f);
+        b_TTall5->translate(24.f,0.1f,16.f);
+        b_TSmall5->translate(16.f,0.1f,24.f);
 
 
     //Player
@@ -237,8 +318,9 @@ Node *initScene1()
         // die x achsendrehung kann noch beeinflusst werden
         v_CharacterWithCam->setCam(dynamic_cast<PhysicAccessableCamera*>(SceneManager::instance()->getActiveContext()->getCamera()));
         // Relative Kameraposition zum Drawable setzen
-     //   v_CharacterWithCam->setRelativeCamPosition(0.f, 4.0f, 6.0f);
-        v_CharacterWithCam->setUpDownView(15.0F);
+        v_CharacterWithCam->setRelativeCamPosition(0.f, 4.0f, 6.0f);
+        //v_CharacterWithCam->setUpDownView(15.0F);
+        v_CharacterWithCam->setUpDownView(-35.0F);
         v_Ball->getPhysicObject()->registerPhysicObject();
 
         new Spieler (v_CharacterWithCam);
@@ -262,7 +344,7 @@ Node *initScene1()
         //Node *lSource1Node = new Node(gSoundSource1);
 
         //NatureAmbience ist Stereo und ändert daher als ambienter Sound seine "Position" nicht
-        gSoundSource1 = new SoundSource(new SoundFile(path+QString("/sound/RollerMobster_edit.wav")));
+        gSoundSource1 = new SoundSource(new SoundFile(path+QString("/sound/RollerMobster.wav")));
       //  gAmbientSoundSource->setLooping(true);
         gSoundSource1->play();
 
@@ -270,6 +352,22 @@ Node *initScene1()
         t = v_Plane->getProperty<Texture>();
         t->loadPicture(path + QString("/modelstextures/Boden_re.jpg"));
         v_Plane->setShader(s);
+
+        t = model1_Fish->getProperty<Texture>();
+        t->loadPicture(path + QString("/modelstextures/Pufferfish_2.jpg"));
+        model1_Fish->setShader(s);
+
+        t = model4_Spawn->getProperty<Texture>();
+        t->loadPicture(path + QString("/modelstextures/image.jpg"));
+        model4_Spawn->setShader(s);
+
+        t = model5_Small->getProperty<Texture>();
+        t->loadPicture(path + QString("/modelstextures/image.jpg"));
+        model5_Small->setShader(s);
+
+        t = model6_Middle->getProperty<Texture>();
+        t->loadPicture(path + QString("/modelstextures/image.jpg"));
+        model6_Middle->setShader(s);
 
 
 
@@ -299,6 +397,26 @@ Node *initScene1()
         transformationBuildingSpawn3->addChild(transformationBuildingSpawn5);
         transformationBuildingSpawn5->addChild(new Node(model11_SDoor));
        // transformationBuildingSpawn5->addChild(new Node(gSoundSource1));
+        root->addChild(transformationBuilding4);
+        transformationBuilding4->addChild(new Node(model12_Small2));
+        root->addChild(transformationBuilding5);
+        transformationBuilding5->addChild(new Node(model13_Middle2));
+        root->addChild(transformationBuilding6);
+        transformationBuilding6->addChild(new Node(model14_Tall2));
+        root->addChild(transformationBuilding7);
+        transformationBuilding7->addChild(new Node(model15_Tall3));
+        root->addChild(transformationBuilding8);
+        transformationBuilding8->addChild(new Node(model16_Tall4));
+        root->addChild(transformationBuilding9);
+        transformationBuilding9->addChild(new Node(model17_Small3));
+        root->addChild(transformationBuilding10);
+        transformationBuilding10->addChild(new Node(model18_Small4));
+        root->addChild(transformationBuilding11);
+        transformationBuilding11->addChild(new Node(model19_Middle3));
+        root->addChild(transformationBuilding12);
+        transformationBuilding12->addChild(new Node(model20_Tall5));
+        root->addChild(transformationBuilding13);
+        transformationBuilding13->addChild(new Node(model21_Small5));
 
 
 
